679d360bde9f6a51dc2273b001c90091e1643cdd
Duo Tao <taod@carleton.edu>
2016-06-10 17:45:10 -0500

    finished edit UI

--- src/sample/MainController.java
+++ src/sample/MainController.java
@@ -12,12 +12,18 @@ import java.util.regex.Pattern;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
 public class MainController {
     private static final String TASKS_PATH = ".tasks";
+    private static final int EDIT_WINDOW_WIDTH = 300;
+    private static final int EDIT_WINDOW_HEIGHT = 100;
     private static Tasks tasks = null;
 
     @FXML
@@ -26,8 +32,6 @@ public class MainController {
     public Button checkButton;
     @FXML
     public Button resetButton;
-    @FXML
-    public Button uncheckButton;
     @FXML
     public AnchorPane mainScene;
     @FXML
@@ -86,8 +90,16 @@ public class MainController {
     }
 
     @FXML
-    public void onUncheckButtonClick() {
-        System.out.println("uncheck");
+    public void onEditButtonClick() throws IOException{
+        Parent root = FXMLLoader.load(getClass().getResource("EditView.fxml"));
+        Stage editStage = new Stage();
+        editStage.setTitle("Edit Tasks");
+        editStage.setScene(new Scene(
+                root, EDIT_WINDOW_WIDTH, EDIT_WINDOW_HEIGHT));
+        editStage.sizeToScene();
+        editStage.setResizable(false);
+        editStage.initOwner(this.taskListView.getScene().getWindow());
+        editStage.show();
     }
 
     private void update() {
@@ -108,7 +120,6 @@ public class MainController {
 
     public static void saveStatus() throws IOException {
         FileWriter fw = new FileWriter(TASKS_PATH, false);
-        System.out.print(tasks.getTaskDict().keySet().size());
         for (String taskName : tasks.getTaskDict().keySet()) {
             fw.write(taskName);
             if (tasks.getTaskDict().get(taskName).checked) {
